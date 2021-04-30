e93a050e9f3174c23ab5e2502a863d643f99372e
Erlend Hamnaberg <erlend@hamnaberg.net>
2021-04-30 11:00:42 +0200

    Prime for release

--- build.sbt
+++ build.sbt
@@ -1,17 +1,33 @@
 inThisBuild(
   Seq(
+    organization := "net.hamnaberg",
     scalaVersion := "2.13.5",
     testFrameworks += new TestFramework("munit.Framework"),
     addCompilerPlugin(("org.typelevel" % "kind-projector" % "0.11.3").cross(CrossVersion.full))
   ))
 
-lazy val core = project.settings(
-  libraryDependencies ++= Seq(
-    "com.softwaremill.sttp.tapir" %% "tapir-openapi-model" % "0.17.19",
-    "com.softwaremill.sttp.tapir" %% "tapir-openapi-circe" % "0.17.19",
-    "io.circe" %% "circe-core" % "0.13.0",
-    "org.typelevel" %% "cats-core" % "2.6.0",
-    "org.typelevel" %% "cats-free" % "2.6.0",
-    "org.scalameta" %% "munit" % "0.7.25" % Test
+lazy val core = project
+  .settings(CommonSettings.settings)
+  .settings(
+    name := "openapi-schema-core",
+    libraryDependencies ++= Seq(
+      "com.softwaremill.sttp.tapir" %% "tapir-openapi-model" % "0.17.19",
+      "com.softwaremill.sttp.tapir" %% "tapir-openapi-circe" % "0.17.19",
+      "io.circe" %% "circe-core" % "0.13.0",
+      "org.typelevel" %% "cats-core" % "2.6.0",
+      "org.typelevel" %% "cats-free" % "2.6.0",
+      "org.scalameta" %% "munit" % "0.7.25" % Test
+    )
   )
-)
+
+lazy val root =
+  project
+    .in(file("."))
+    .settings(CommonSettings.settings)
+    .settings(
+      name := "openapi-schema",
+      publishArtifact := false,
+      releaseCrossBuild := true,
+      releaseVersionBump := sbtrelease.Version.Bump.Minor
+    )
+    .aggregate(core)
---
+++ project/CommonSettings.scala
@@ -0,0 +1,62 @@
+import sbt._, Keys._
+import com.jsuereth.sbtpgp.PgpKeys
+import sbtrelease.ReleasePlugin.autoImport._
+
+object CommonSettings {
+  val settings = Seq(
+    Compile / compile / scalacOptions ++= Seq("-release", "8"),
+    Test / scalacOptions ++= Seq("-release", "11"),
+    publishTo := {
+      if (isSnapshot.value)
+        Some(Opts.resolver.sonatypeSnapshots)
+      else
+        Some(Opts.resolver.sonatypeStaging)
+    },
+    releasePublishArtifactsAction := PgpKeys.publishSigned.value,
+    packageOptions += {
+      val title = name.value
+      val ver = version.value
+      val vendor = organization.value
+
+      Package.ManifestAttributes(
+        "Created-By" -> "Scala Build Tool",
+        "Built-By" -> System.getProperty("user.name"),
+        "Build-Jdk" -> System.getProperty("java.version"),
+        "Specification-Title" -> title,
+        "Specification-Version" -> ver,
+        "Specification-Vendor" -> vendor,
+        "Implementation-Title" -> title,
+        "Implementation-Version" -> ver,
+        "Implementation-Vendor-Id" -> vendor,
+        "Implementation-Vendor" -> vendor
+      )
+    },
+    credentials ++= Seq(
+      Credentials(Path.userHome / ".sbt" / ".credentials")
+    ),
+    publishMavenStyle := true,
+    Test / publishArtifact := false,
+    pomIncludeRepository := { _ =>
+      false
+    },
+    homepage := Some(url("https://github.com/hamnis/openapi-schema")),
+    startYear := Some(2021),
+    licenses := Seq(
+      "Apache2" -> url("https://github.com/hamnis/openapi-schema/blob/master/LICENSE")
+    ),
+    scmInfo := Some(
+      ScmInfo(
+        new URL("https://github.com/hamnis/openapi-schema"),
+        "scm:git:git@github.com:hamnis/openapi-schema.git",
+        Some("scm:git:git@github.com:hamnis/openapi-schema.git")
+      )),
+    developers ++= List(
+      Developer(
+        "hamnis",
+        "Erlend Hamnaberg",
+        "erlend@hamnaberg.net",
+        new URL("http://twitter.com/hamnis")
+      )
+    )
+  )
+}
