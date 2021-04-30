8ed7722016bbb83157a545bd9b6a1b15275c8176
Erlend Hamnaberg <erlend@hamnaberg.net>
2021-04-30 15:16:12 +0200

    Add reference to schema

--- core/src/main/scala/net/hamnaberg/schema/Components.scala
+++ core/src/main/scala/net/hamnaberg/schema/Components.scala
@@ -1,10 +1,14 @@
 package net.hamnaberg.schema
 
+import sttp.tapir.apispec.Reference
 import sttp.tapir.openapi.{Components => TapirComponents}
 
 import scala.collection.immutable.ListMap
 
 case class Components(underlying: TapirComponents = TapirComponents(ListMap.empty, ListMap.empty)) {
-  def addSchema[A: Schema](name: String) =
+  def addSchema[A: Schema](name: String): Components =
     Components(underlying.copy(schemas = underlying.schemas.updated(name, Right(Schema[A].compiled))))
+
+  def referenceToSchema(name: String): Option[Reference] =
+    underlying.schemas.get(name).map(_ => Reference(s"#/components/schemas/$name"))
 }
